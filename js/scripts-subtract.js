4293b4c6795b7b9a8f1ab3d62c138a588070a949
Student <example@example.com>
2019-01-30 19:58:05 -0800

    Set up calculator functions for addition, subtraction, multiplication, and division

---
+++ js/scripts-subtract.js
@@ -0,0 +1,8 @@
+var subtract = function(number1, number2) {
+  return number1 - number2;
+}
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter a number to subtract:"));
+var result = subtract(number1, number2);
+alert(result);
