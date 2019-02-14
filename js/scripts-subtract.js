265ebb52a0bd166b5670aa247daa8252152aa1f0
Imandra McKenzie <indigoimandra@gmail.com>
2019-02-14 15:45:53 -0800

    Add calculator with 4 button and working js functions along with updates styles.

--- js/scripts-subtract.js
+++
@@ -1,8 +0,0 @@
-var subtract = function(number1, number2) {
-  return number1 - number2;
-}
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter a number to subtract:"));
-var result = subtract(number1, number2);
-alert(result);
---
+++ js/scripts.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+  $("#add-value").hide();
+  $("#sub-value").hide();
+  $("#mult-value").hide();
+  $("#divi-value").hide();
+
+  var addedNums = function(numberOne, numberTwo) {
+      return numberOne + numberTwo;
+    };
+  var subNums = function(numberOne, numberTwo) {
+      return numberOne - numberTwo;
+    };
+  var multNums = function(numberOne, numberTwo) {
+  return numberOne * numberTwo;
+  };
+  var diviNums = function(numberOne, numberTwo) {
+    return numberOne / numberTwo;
+  };
+
+  $("button").click(function(){
+    if ($(this).attr("id") == "add-button"){
+      var numOneInput = parseInt($("input#numone").val());
+      var numTwoInput = parseInt($("input#numtwo").val());
+      $("#sub-value").hide();
+      $("#mult-value").hide();
+      $("#divi-value").hide();
+      $("#add-value").show();
+      $("#add-total").text(addedNums(numOneInput, numTwoInput));
+    } else if ($(this).attr("id") == "sub-button"){
+      var numOneInput = parseInt($("input#numone").val());
+      var numTwoInput = parseInt($("input#numtwo").val());
+      $("#add-value").hide();
+      $("#mult-value").hide();
+      $("#divi-value").hide();
+      $("#sub-value").show();
+      $("#sub-total").text(subNums(numOneInput, numTwoInput));
+    } else if ($(this).attr("id") == "mult-button"){
+      var numOneInput = parseInt($("input#numone").val());
+      var numTwoInput = parseInt($("input#numtwo").val());
+      $("#add-value").hide();
+      $("#sub-value").hide();
+      $("#divi-value").hide();
+      $("#mult-value").show();
+      $("#mult-total").text(multNums(numOneInput, numTwoInput));
+    } else if ($(this).attr("id") == "divi-button"){
+      var numOneInput = parseInt($("input#numone").val());
+      var numTwoInput = parseInt($("input#numtwo").val());
+      $("#add-value").hide();
+      $("#sub-value").hide();
+      $("#mult-value").hide();
+      $("#divi-value").show();
+      $("#divi-total").text(diviNums(numOneInput, numTwoInput));
+    };
+  });
+});
