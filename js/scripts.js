423caaf24a5172518e683ba9bed223161d2c4337
Imandra McKenzie <indigoimandra@gmail.com>
2019-02-19 14:54:59 -0800

    update html and js formatting

--- js/scripts.js
+++ js/scripts.js
@@ -21,25 +21,25 @@ $(document).ready(function(){
     var numOneInput = parseInt($("input#numone").val());
     var numTwoInput = parseInt($("input#numtwo").val());
 
-    if ($(this).attr("id") == "add-button" && $.isNumeric(numOneInput) && $.isNumeric(numTwoInput)){
+    if ($(this).attr("id") == "add-button" && $.isNumeric(numOneInput) && $.isNumeric(numTwoInput)) {
       $("#sub-value").hide();
       $("#mult-value").hide();
       $("#divi-value").hide();
       $("#add-value").show();
       $("#add-total").text(addedNums(numOneInput, numTwoInput));
-    } else if ($(this).attr("id") == "sub-button" && $.isNumeric(numOneInput) && $.isNumeric(numTwoInput)){
+    } else if ($(this).attr("id") == "sub-button" && $.isNumeric(numOneInput) && $.isNumeric(numTwoInput)) {
       $("#add-value").hide();
       $("#mult-value").hide();
       $("#divi-value").hide();
       $("#sub-value").show();
       $("#sub-total").text(subNums(numOneInput, numTwoInput));
-    } else if ($(this).attr("id") == "mult-button" && $.isNumeric(numOneInput) && $.isNumeric(numTwoInput)){
+    } else if ($(this).attr("id") == "mult-button" && $.isNumeric(numOneInput) && $.isNumeric(numTwoInput)) {
       $("#add-value").hide();
       $("#sub-value").hide();
       $("#divi-value").hide();
       $("#mult-value").show();
       $("#mult-total").text(multNums(numOneInput, numTwoInput));
-    } else if ($(this).attr("id") == "divi-button" && $.isNumeric(numOneInput) && $.isNumeric(numTwoInput)){
+    } else if ($(this).attr("id") == "divi-button" && $.isNumeric(numOneInput) && $.isNumeric(numTwoInput)) {
       $("#add-value").hide();
       $("#sub-value").hide();
       $("#mult-value").hide();
